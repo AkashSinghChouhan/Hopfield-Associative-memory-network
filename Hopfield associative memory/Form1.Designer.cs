11a2935062d4de7833cacbcf0e7fd877cf8724d0
AkashSinghChouhan <37949976+AkashSinghChouhan@users.noreply.github.com>
2019-05-05 04:01:55 +0200

    weight updation

--- Hopfield associative memory/Form1.Designer.cs
+++ Hopfield associative memory/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -58,6 +59,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Generate output";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -103,7 +105,7 @@
             // button4
             // 
             this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button4.Location = new System.Drawing.Point(172, 224);
+            this.button4.Location = new System.Drawing.Point(287, 224);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(63, 39);
             this.button4.TabIndex = 6;
@@ -114,7 +116,7 @@
             // button5
             // 
             this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button5.Location = new System.Drawing.Point(256, 225);
+            this.button5.Location = new System.Drawing.Point(129, 224);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(61, 38);
             this.button5.TabIndex = 7;
@@ -122,11 +124,23 @@
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button6.Location = new System.Drawing.Point(213, 226);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(57, 35);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "0";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1023, 621);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
@@ -153,6 +167,7 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Hopfield associative memory/Form1.cs
+++ Hopfield associative memory/Form1.cs
@@ -14,20 +14,26 @@ namespace Hopfield_associative_memory
     
     public partial class Form1 : Form
     {
-        static int i = 0, j = 0;
+        
+        static int i = 0;
         List<List<int>> vec;
+        List<int> temp;
+        int [,]weight;
 
         public Form1()
         {
-            InitializeComponent();
             vec = new List<List<int>>();
+            temp = new List<int>();
+            
+            InitializeComponent();
+            
             
         }
-
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -41,20 +47,62 @@ namespace Hopfield_associative_memory
 
         private void button1_Click(object sender, EventArgs e)
         {
+            vec.Add(temp);
             ++i;
+            
+            textBox1.Clear();
+            //temp.Clear();
+            temp = new List<int>(); 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.AppendText("1 ");
-            vec.ElementAt(i).Add(1);
-            
+            textBox1.AppendText(" 1 ");
+            temp.Add(1);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            textBox1.AppendText("-1 ");
-            vec.ElementAt(i).Add(-1);
+            textBox1.AppendText(" -1 ");
+            temp.Add(-1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(" 0 ");
+            temp.Add(-1);
+        }
+        public void hopfield() {
+            weight = new int[vec[0].Count,vec[0].Count];
+            for (int x = 0; x < vec.Count; x++)
+            {
+                for (int y = 0; y < weight.GetLength(0); y++)
+                {
+                    for (int z=0 ; z < weight.GetLength(1); z++) {
+
+                        if (y == z) { weight[y, z] = 0; }
+                        else { weight[y, z] += (vec[x][y] * vec[x][z]); }
+                    }
+                }
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string separator = " , ";
+            this.hopfield();
+            StringBuilder sb = new StringBuilder("Weight: ");
+            for (int y = 0; y < weight.GetLength(0); y++)
+            {
+                for (int z = 0; z < weight.GetLength(1); z++)
+                {
+
+                    sb.Append(weight[y,z].ToString()+" ");
+                }
+                sb.Append("\n");
+            }
+            textBox2.Text = sb.ToString();
         }
     }
 }
